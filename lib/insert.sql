e8dae22fcc9c01f53056c2488550fd23e6e6206f
Akisa-Moruri <akisamoruri@gmail.com>
2024-06-04 11:09:42 +0300

    bears lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ( 'Mr. Chocolate', 32, 'M', 'Green', 'calm', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ( 'Rowdy', 28, 'M', 'Blue', 'Energetic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ( 'Tabitha', 45, 'F', 'Red', 'Assertive', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ( 'Sergeant Brown', 37, 'M', 'Friendly', 'Slimy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ( 'Melissa', 22, 'F', 'Purple', 'Introverted', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ( 'Grinch', 5, 'M', 'Pink', 'Wise', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ( 'Wendy', 29, 'F', 'Black', 'Confident', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ( null, 11, 'M', 'White', 'Caring', 0);
